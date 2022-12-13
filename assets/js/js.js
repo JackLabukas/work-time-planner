b60bc6c0e464ed18aa534d1a13a73bce2c6b7dc5
JackLabukas <labukasgediminas@gmail.com>
2022-12-13 13:22:36 +0000

    added button functionality

--- assets/js/js.js
+++ assets/js/js.js
@@ -5,7 +5,7 @@ $(document).ready(function () {
   // currenrt hour
   var hour = moment().format("HH");
   console.log(hour);
-
+  // compare each time slot 'id' to actual time
   $(".time-block").each(function () {
     var timeDiv = $(this).attr("id").split("-")[1];
     // console.log(timeDiv);
@@ -20,4 +20,24 @@ $(document).ready(function () {
       $(this).addClass("past");
     }
   });
+  // when button is clicked we get the value of sibling element in this case textarea with class .description
+  // and get time of when button is clicked of parents id attr value
+  $(".saveBtn").click(function (event) {
+    event.preventDefault();
+    var value = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id").split("-")[1];
+    console.log(value);
+    localStorage.setItem(time, value);
+  });
+
+  // sat values to textfields of html page that we get from localStorage
+  $("#hour-09 .description").val(localStorage.getItem("09"));
+  $("#hour-10 .description").val(localStorage.getItem("10"));
+  $("#hour-11 .description").val(localStorage.getItem("11"));
+  $("#hour-12 .description").val(localStorage.getItem("12"));
+  $("#hour-13 .description").val(localStorage.getItem("13"));
+  $("#hour-14 .description").val(localStorage.getItem("14"));
+  $("#hour-15 .description").val(localStorage.getItem("15"));
+  $("#hour-16 .description").val(localStorage.getItem("16"));
+  $("#hour-17 .description").val(localStorage.getItem("17"));
 });
